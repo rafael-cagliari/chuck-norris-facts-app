16af693370522025c90565f018bdd946b9ab26bb
rafael-cagliari <rafael.cagliari22@gmail.com>
2021-06-20 00:59:32 -0300

    -fixed koin version

--- app/src/main/java/com/rafael/chuck_norris/viewmodel/FactListViewModel.kt
+++ app/src/main/java/com/rafael/chuck_norris/viewmodel/FactListViewModel.kt
@@ -3,24 +3,15 @@ package com.rafael.chuck_norris.viewmodel
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.rafael.data.datasource.DataBaseDataSourceImpl
-import com.rafael.data.datasource.GetFactDataSourceImpl
-import com.rafael.data.network.RetrofitInstance
-import com.rafael.data.persistance.FactsDataBase
-import com.rafael.data.repository.DataBaseRepositoryImpl
-import com.rafael.data.repository.GetFactRepositoryImpl
 import com.rafael.domain.model.ChuckNorrisFact
 import com.rafael.domain.usecase.*
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-@KoinApiExtension
 class FactListViewModel(val deleteFactFromDBUseCase:DeleteFactFromDBUseCase,
                         val readAllDBUseCase:ReadAllDBUseCase):ViewModel(), KoinComponent {
 
